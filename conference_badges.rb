84f0674ca80f7131c66c2acc34e9bccd843803eb
Mei Wong <fongmeiw@gmail.com>
2019-10-09 04:47:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
 # Write your code here.
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
   i = 0
+  new_array = []
   while i < array.size do
     new_array.push(badge_maker(array[i]))
   end
