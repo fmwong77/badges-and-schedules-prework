763110d6810bf3797bc27400ed3685e452143f17
Mei Wong <fongmeiw@gmail.com>
2019-10-09 04:50:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,13 +14,13 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
-  i = 0
-  new_array = []
-  while i < array.size do
-    new_array.push(badge_maker(array[i]))
-    i += 1
-  end
-  new_array
+  # i = 0
+  # new_array = []
+  # while i < array.size do
+  #   new_array.push(badge_maker(array[i]))
+  #   i += 1
+  # end
+  # new_array
 end
 
 def printer()
